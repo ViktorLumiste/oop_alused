034753e60e378b9483bc8459c736f8b8dc8ec3c9
Viktor Lumiste <viktor.lumiste@khk.ee>
2022-01-20 10:55:44 +0200

    tootajad ul8

--- praks2/tootaajad.py
+++ praks2/tootaajad.py
@@ -8,9 +8,12 @@ inimene2.tutvustus()
 inimene3.tutvustus()
 
 if inimene1.kutse_kval < inimene2.kutse_kval and inimene1.kutse_kval < inimene3.kutse_kval:
+    print(inimene1.enimi,inimene1.pnimi, ", te olite vallandatud")
     del inimene1
 elif inimene2.kutse_kval < inimene1.kutse_kval and inimene2.kutse_kval < inimene3.kutse_kval:
+    print(inimene2.enimi, inimene2.pnimi, ", te olite vallandatud")
     del inimene2
 elif inimene3.kutse_kval < inimene1.kutse_kval and inimene3.kutse_kval < inimene2.kutse_kval:
+    print(inimene3.enimi, inimene3.pnimi, ", te olite vallandatud")
     del inimene3
 input()
