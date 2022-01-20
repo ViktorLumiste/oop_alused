4d8a66bf678e90742e0929b68dc12828775bfbbd
Viktor Lumiste <viktor.lumiste@khk.ee>
2022-01-20 11:18:49 +0200

    restoran2 ul9

---
+++ praks2/restoran2.py
@@ -0,0 +1,10 @@
+from restoraan2 import Restoraan
+
+restoran1 = Restoraan("Olafi Resto", "su ema")
+restoran1.kirjelda_restoran()
+restoran1.ava_restoraan()
+print(restoran1.teen_kyl)
+restoran1.maara_teenindatud_kyl(13)
+print(restoran1.teen_kyl)
+restoran1.suurenda_teen_kyl(12)
+print(restoran1.teen_kyl)
