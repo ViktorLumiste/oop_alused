23d61608ab486a3ce66f9f324f418bcb127a27ad
Viktor Lumiste <viktor.lumiste@khk.ee>
2022-01-20 09:47:54 +0200

    restoran1 ul6

--- praks2/restoran1.py
+++ praks2/restoran1.py
@@ -1,6 +1,6 @@
 from restoraan1 import Restoraan
-olafi_resto = restoran1("Garfieldi restoran", "Lasagna")
+olafi_resto = Restoraan("Garfieldi restoran", "Lasagna")
 
-olafi_resto.kirjelda_restoran
-olafi_resto.ava_restoraan
+olafi_resto.kirjelda_restoran()
+olafi_resto.ava_restoraan()
 
